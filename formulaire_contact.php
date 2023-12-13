0af528931f0109dde689d4a4ffebba5bdb2b8a1f
Sandrine <sandrineblandamour@gmail.com>
2023-12-13 18:06:21 +0100

    Traitement formulaire POST

---
+++ formulaire_contact.php
@@ -0,0 +1,28 @@
+<body>
+<section class="border-top pb-5 mb-5 container">
+    
+    <h2 class="text-center display-4 my-md-5 my-4">Contact-us</h2>
+    
+        <form method="POST" action="traitement_formulaire.php">
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" id="email" class="form-control">
+                <p class="form-text m-0">We'll never share your email.</p>
+            </div>
+            
+            <div class="form-floating mb-3">
+                <textarea
+                class="form-control"
+                placeholder="Your message"
+                id="floatTxtArea"
+                style="height: 100px"
+                ></textarea>
+                <label for="floatTxtArea">Your message</label>
+            </div>
+            
+            <button class="btn btn-primary">Submit</button>
+        </form>
+    
+</section>
+</body>
--- index.php
+++ index.php
@@ -317,27 +317,8 @@
 <section class="border-top pb-5 mb-5 container">
     
     <h2 class="text-center display-4 my-md-5 my-4">Contact-us</h2>
-    
-    <form>
-        
-        <div class="mb-3">
-            <label for="email" class="form-label">Email address</label>
-            <input type="email" id="email" class="form-control">
-            <p class="form-text m-0">We'll never share your email.</p>
-        </div>
-        
-        <div class="form-floating mb-3">
-            <textarea
-            class="form-control"
-            placeholder="Your message"
-            id="floatTxtArea"
-            style="height: 100px"
-            ></textarea>
-            <label for="floatTxtArea">Your message</label>
-        </div>
-        
-        <button class="btn btn-primary">Submit</button>
-    </form>
+
+    <?php include 'index.php/formulaire_contact.php'; ?>
     
 </section>
 
