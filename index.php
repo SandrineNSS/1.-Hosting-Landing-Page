5e5967eb8d3db1e0ec4fe34b129884819e38a5a1
Sandrine <sandrineblandamour@gmail.com>
2023-12-17 16:54:08 +0100

    Modif footer href

--- index.php
+++ index.php
@@ -326,7 +326,7 @@
 <!-- Footer -->
 
 <footer class="bg-light text-center py-3 my-4 border-top">
-    <a href="#" class="mb-3 mb-md-0 text-muted text-decoration-none lh-1">© 2021 Company, Inc</a>
+    <a href="#" class="mb-3 mb-md-0 text-muted text-decoration-none lh-1">© 2023 Company, Inc</a>
 </footer>
 
 
